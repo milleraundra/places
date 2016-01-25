8f6941b7d05d501a13678acd05f708c68bb7f533
aundra miller and mike gallegos <miller.aundra@gmail.com>
2016-01-25 10:02:15 -0800

    final commit

--- spec/specs.js
+++ spec/specs.js
@@ -1,4 +1,12 @@
-
+describe('Place', function() {
+	it("creates a new Place object with the given properties", function (){
+		var testPlace = new Place("Australia","opera house","fall","Australia is cool")
+		expect(testPlace.location1).to.equal("Australia");
+		expect(testPlace.landmark).to.equal("opera house");
+		expect(testPlace.timeOfYear).to.equal("fall");
+		expect(testPlace.note).to.equal("Australia is cool");
+	});
+});
 
 
 
